0432b73cd19df5be4a46b884df5af46047703ac4
Lucas Gabriel <gabrielplaygt@gmail.com>
2019-06-12 15:29:11 -0300

    Crud

---
+++ completecrud/main.js
@@ -0,0 +1,39 @@
+
+let id = $("input[name*='book_id']")
+id.attr("readonly","readonly");
+
+
+$(".btnedit").click(e => {
+	let textvalues = displayData(e);	
+
+	
+	let bookname = $("input[name*='book_name']");
+	let bookpublisher = $("input[name*='book_publisher']");
+	let bookprice = $("input[name*='book_price']");
+
+	id.val(textvalues[0]);
+	bookname.val(textvalues[1]);
+	bookpublisher.val(textvalues[2]);
+	bookprice.val(textvalues[3].replace("$", ""));
+
+	//console.log(bookname.val());
+
+	//vai ao topo da tela
+	// $('html,body').scrollTop(0);
+	$('html, body').animate({scrollTop:0}, 'slow'); //slow, medium, fast
+});
+
+function displayData(e) {
+	let id = 0;
+	const td = $("#tbody tr td");
+	let textvalues = [];
+
+	for (const value of td) {
+		if (value.dataset.id == e.target.dataset.id) {
+			textvalues[id++] = value.textContent;
+	}
+}
+
+return textvalues;
+
+}
