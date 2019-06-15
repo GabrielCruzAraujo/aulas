7214535ceb74aeea043efbc7915919a1da46589a
Lucas Gabriel <gabrielplaygt@gmail.com>
2019-06-15 12:06:09 -0300

    1206

---
+++ Novo1206/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Livros</title>
+
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css">
+
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+	<link rel="stylesheet"  href="style.css">
+
+</head>
+<body>
+	<main>
+		<div class="container text-center">
+			<h1 class="py-4 bg-dark text-light rounded"><i class="fas fa-swatchbook"></i> Livraria</h1>
+
+			<div class="d-flex justify-content-center">
+				<form action="" method="post" class="w-50">
+					
+					<div class="pt-2"></div>
+					<div class="pt-2"></div>
+					<div class="row pt-2"></div>
+					<div class="col"></div>
+					<div class="col"></div>
+
+			</div>
+		</div>
+		<div class="d-flex justify-content-center"></div>
+	</form>
+		<div class="d-flex table-data">
+				<table class="table table-striped table-dark">
+					<thead class="thead-dark">
+						<tr>
+							<th>ID</th>
+							<th>Nome do Livro</th>
+							<th>Autor</th>
+							<th>Preço do Livro</th>
+							<th>Editar</th>
+						</tr>
+					</thead>
+					<tbody id="tbody"></tbody>
+				</table>
+
+	</main>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	
+
+</body>
+</html>
