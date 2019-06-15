724833500d8fcc8d2495fdbf4d76046f5140c58a
Lucas Gabriel <gabrielplaygt@gmail.com>
2019-06-15 12:05:10 -0300

    link

---
+++ link/Link.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Links</title>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,500,500i,600,600i,700,700i,800,900&display=swap" rel="stylesheet">
+	<style type="text/css">
+		* {
+			margin:0;
+			padding: 0;
+			font-family: "Montserrat", sans-serif;
+			font-weight: 600;
+			color: #109fca;
+		}
+		.conteudo {
+			width: 100%;
+			height: auto;
+		}
+		.item {
+			width: 100%;
+			max-width: 400px;
+			margin: 0 auto;
+			margin-top: 100px;
+			
+
+		}
+
+		.item a {
+			line-height: 50px;
+			width: 100%;
+			display: block;
+			text-align: center;
+			text-decoration: none;
+			text-transform: uppercase;
+			transition: 0.2s;
+		}
+
+		.item a:hover {
+			background: #109fff;
+			color: #fff;
+
+		}
+	</style>
+</head>
+<body>
+	<div class="conteudo">
+		<div class="item">
+			<a href="../ponto/index.php">Ponto</a>
+			<a href="../novoformuláriocadastro/index.php">Novo Formulário</a>
+			<a href="../fac/index.php">Fac</a>
+			<a href="../completecrud/index.php">Crud</a>
+			<a href="../instituicao/index.php">Instituição</a>
+		</div>
+	</div>
+</body>
+</html>
